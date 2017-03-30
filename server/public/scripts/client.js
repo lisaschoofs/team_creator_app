3374fb20bf8a59582cad2d524b548fe001d61fff
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-30 10:06:07 -0500

    code broken

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,45 +3,70 @@ $(document).ready(function() {
 
     getStudentNames();
 
-    generateTeams();
+  
+    generateBtn();
 
 
 }); //end doc ready
 
 
 
+function generateBtn() {
+    $('.inputForm').on('submit', function(event) {
+        event.preventDefault();
+        console.log('teams');
+        var teamSize = $('.inputNeededGroups').val();
 
-
-function saveTeamsToDB() {
-    //  AJAX POST to send the formulated teams
-    //need object with generated teams into an {object: team1: name...}
-    $.ajax({
-        type: 'POST',
-        url: '/teams/save',
-        data: {
-            teams: x
-        },
-        success: function(response) {
-            console.log(response);
+        for (var i = 0; i < teamSize; i++) {
+            console.log(teamSize);
+            $('#output').append('<div class="row"><div class="col-md-12">Teams</div></div>');
         }
+        $('.inputNeededGroups').val('');
     });
+    getStudentNames();
 }
 
 
-// function generateBtn() {
-//     $('.inputForm').on('submit', function(event) {
-//         event.preventDefault();
-//         console.log('teams');
-//         var teamSize = $('.inputNeededGroups').val();
-//
-//         for (var i = 0; i < teamSize; i++) {
-//             console.log(teamSize);
-//             $('#output').append('<div class="row"><div class="col-md-12">' + teamOne + teamTwo '</div></div>');
-//         }
-//         $('.inputNeededGroups').val('');
-//     });
-//     getStudentNames();
-// }
+function appendStudents(response) {
+    var reMix = [];
+    $("#output").empty();
+    for (var i = 0; i < response.length; i++) {
+        reMix.push(response[i].name);
+    }
+    // console.log(reMix);
+    // console.log(shuffleArray(reMix));
+
+    var randomArray = shuffleArray(reMix);
+   for(var p = 0; p < randomArray.length; p++) {
+     $("#output").append('<p>' + randomArray[p] + '</p>');
+}
+
+
+function getStudentNames() {
+    //AJAx GET REquuest
+    $.ajax({
+        type: 'GET',
+        url: '/teams',
+        success: function(response) {
+            // console.log(response);
+            appendStudents(response);
+        }
+    }); //end Ajax
+} //end function
+
+
+function shuffleArray(array) {
+    //logic for generating random teams
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+}
+
 
 function generateTeams(randomArray) {
     $('.inputForm').on('submit', function(event) {
@@ -132,38 +157,17 @@ function generateTeams(randomArray) {
     });
 } // end function generateTeams
 
-function appendStudents(response) {
-    var reMix = [];
-    $("#output").empty();
-    for (var i = 0; i < response.length; i++) {
-        reMix.push(response[i].name);
-    }
-    // console.log(reMix);
-    // console.log(shuffleArray(reMix));
-    var randomArray = shuffleArray(reMix);
-}
-
-
-function getStudentNames() {
-    //AJAx GET REquuest
+function saveTeamsToDB() {
+    //  AJAX POST to send the formulated teams
+    //need object with generated teams into an {object: team1: name...}
     $.ajax({
-        type: 'GET',
-        url: '/teams',
+        type: 'POST',
+        url: '/teams/save',
+        data: {
+            teams: x
+        },
         success: function(response) {
-            // console.log(response);
-            appendStudents(response);
+            console.log(response);
         }
-    }); //end Ajax
-} //end function
-
-
-function shuffleArray(array) {
-    //logic for generating random teams
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
+    });
 }
