1a4bdb0f8af469be5b69d2888befb913f6f0476d
Betsy Rowley <BRowley8@gmail.com>
2017-03-29 22:08:33 -0500

    ShuffleArray function created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,42 +1,58 @@
 $(document).ready(function(){
+  console.log("jq sourced");
 
+getStudentNames();
 
 }); //end doc ready
 
 
-function appendTeams(names) {
-  $('div to append to').empty();
-  for (var i = 0; i < response.length; i++) {
-  var name = response[i];
-  appendStudents(name);
-  }
-}
+// function appendTeams(response) {
+//
+//   $('#output').empty();
+//   for (var i = 0; i < response.length; i++) {
+//   var name = response[i];
+//
+//   appendStudents(name);
+//   }
+// }
 
 
-function appendStudents(student) {
-$('div to append to').empty();
-var $el = $('div to append to').children().last();
-$el.append('<p>' + student.name + '</p>');
-
-
+function appendStudents(response) {
+  var reMix = [];
+  $("#output").empty();
+  for(var i = 0; i < response.length; i++) {
+  reMix.push(response[i].name);
+    }
+    console.log(reMix);
+    console.log(shuffleArray(reMix));
+    var newArray = shuffleArray(reMix);
+    for(var p = 0; p < newArray.length; p++) {
+      $("#output").append('<p>' + newArray[p] + '</p>');
+    }
 }//end func
 
 function getStudentNames() {
   //AJAx GET REquuest
-$.Ajax({
+$.ajax({
   type:'GET',
   url: '/teams',
   success: function(response) {
     console.log(response);
-    // appendStudents(response);
+    appendStudents(response);
   }
 });//end Ajax
 }//end function
 
 
-function functionName() {
+function shuffleArray(array) {
   //logic for generating random teams
-
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+      return array;
 }
 
 //append containers for number of teams.....dynamically create depending on input val()
